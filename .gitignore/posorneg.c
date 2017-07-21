7054ff1fb90acfef6db576712e8378932f45f8d6
kesavanraja <kesav.kraja@gmail.com>
2017-07-21 17:49:45 +0530

    Update and rename .gitignore to posorneg.c

---
+++ .gitignore/posorneg.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("it is a positive number");
+}
+else(a<0)
+{
+printf("it is a negative number");
+}
+else
+{
+printf("it is zero");
+}
+return o;
+}
